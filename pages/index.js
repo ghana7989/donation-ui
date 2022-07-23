73eac5e10c60c5afb5261aa7790af36c654e83ce
Pavan <pavan.chindukuri@mosaicwellness.in>
2022-07-24 04:12:36 +0530

    fixing bugs

--- pages/index.js
+++ pages/index.js
@@ -1,329 +1,40 @@
-import Head from "next/head";
-import { useContext } from "react";
-import styles from "../styles/Home.module.css";
-import { MetamaskContext } from "../helpers/ConnectMetamask";
-
-const ERC20_ABI = [
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "name_",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "symbol_",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "owner",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "value",
-        "type": "uint256"
-      }
-    ],
-    "name": "Approval",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "value",
-        "type": "uint256"
-      }
-    ],
-    "name": "Transfer",
-    "type": "event"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "owner",
-        "type": "address"
-      },
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      }
-    ],
-    "name": "allowance",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "approve",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "account",
-        "type": "address"
-      }
-    ],
-    "name": "balanceOf",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "decimals",
-    "outputs": [
-      {
-        "internalType": "uint8",
-        "name": "",
-        "type": "uint8"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "subtractedValue",
-        "type": "uint256"
-      }
-    ],
-    "name": "decreaseAllowance",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "spender",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "addedValue",
-        "type": "uint256"
-      }
-    ],
-    "name": "increaseAllowance",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "name",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "symbol",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "totalSupply",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "transfer",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
-      },
-      {
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "transferFrom",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-]
-const provider = new ethers.providers.JsonRpcProvider( "https://mainnet.infura.io/v3/" )
-const address =  "0xE21c35a68F87EC94776A3f164B8cD0dA284F6A34"
-const contract = new ethers.Contract( address, ERC20_ABI, provider)
+import Head from 'next/head';
+import {useContext, useEffect} from 'react';
+import styles from '../styles/Home.module.css';
+import {MetamaskContext} from '../helpers/ConnectMetamask';
+import {ethers} from 'ethers';
+import ABI from '../contracts/CFP.json';
+const provider = new ethers.providers.JsonRpcProvider(
+	'https://rinkeby.infura.io/v3/c39df75a4b804a1cb9d77d5c313eaa83',
+);
+const address = '0xE21c35a68F87EC94776A3f164B8cD0dA284F6A34';
+const contract = new ethers.Contract(address, ABI.abi, provider);
+console.log('contract: ', contract);
 export default function Home() {
+	useEffect(() => {
+		(async () => {
+			const block = await provider.getBlockNumber();
+		})();
+	}, []);
 
+	const {balance} = useContext(MetamaskContext);
 
-  useEffect(() => {
-    (async()=>{
-      const block = await provider.getBlockNumber()
-
-    })()
-  }, []);
-
-  const {balance} = useContext(MetamaskContext);
- 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>CreatiFi</title>
-        <meta name="description" content="CreatiFi" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex">
-        <div className="bg-black-600 w-[25%] p-4 flex flex-col">
-          <div className="font-bold text-2xl">CreatiFi</div>
-          <button>
-            Contract
-          </button>
-          {address}&nbsp;
-          {balance}
-        </div>
-        <div></div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>CreatiFi</title>
+				<meta name='description' content='CreatiFi' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<div className='flex'>
+				<div className='bg-black-600 w-[25%] p-4 flex flex-col'>
+					<div className='text-2xl font-bold'>CreatiFi</div>
+					<button>Contract</button>
+					{address}&nbsp;
+					{balance}
+				</div>
+				<div></div>
+			</div>
+		</div>
+	);
 }
